13f1e3ce920f849739b7d2f0b396923f3796d9db
agthuhein <agthuhein.mm@gmail.com>
2024-10-21 23:17:12 +0200

    List index exercise

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,14 @@
-name = input("What is the applicant name: ")
-degree = input("What is the major in college: ")
-job = input("What is your current occupation:")
-experience = input("How many years of experience do you have? ")
+ex_list = [[0, 2], [4, 6], [8, 10], [12,14]]
 
-print("{} majored in {}, works as a {}, and has {} years of experiences.".format(name, degree, job, experience))
+print(ex_list[0])
+print(ex_list[3][1])
+
+furniture_list = ["chair", "table", "desk", "lamp", "bed"]
+print(furniture_list[-5])
+
+print("Most people own at least {} {}s.".format(ex_list[0][1], furniture_list[0]))
+
+num_list = [0.98, 8.76, 6.54, 4.32]
+print(num_list[1:])
+print(num_list[1:3])
+print(num_list[0:2])
