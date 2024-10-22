169980097397c8b408ace4385a0f38f161c8167b
agthuhein <agthuhein.mm@gmail.com>
2024-10-22 15:24:26 +0200

    basic maths operations

---
+++ basic_Maths_Operations.py
@@ -0,0 +1,17 @@
+multiply = 10 * 5
+minus = 6 - 3
+times = 68.75 * 0
+divide_1 = 100 / 17
+divide_2 = 100 % 17
+power = 2 * 2 * 2
+x = 5
+y = 3
+xy = x * y
+
+print(multiply)
+print(minus)
+print(times)
+print(divide_1)
+print(divide_2)
+print(power)
+print(xy)
