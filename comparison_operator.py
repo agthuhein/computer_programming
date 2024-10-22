520ecada4026eccfa1e0bb0fd615473785e5c18a
agthuhein <agthuhein.mm@gmail.com>
2024-10-22 15:59:04 +0200

    comparison operation exercise

---
+++ comparison_operator.py
@@ -0,0 +1,27 @@
+#print(2 < 1)
+#print(5 > 2.5)
+
+#Traditional for-loop
+output = []
+for x in [10, 5, 6]:
+    if x % 2 == 0:
+        output.append(x)
+
+
+#list conprehension (More "Pythonic")
+output_list = [x for x in [10, 5, 6] if x % 2 == 0]
+
+#############
+x = 10
+y = 15
+
+print(x == y)
+print(x > y)
+print(y > x)
+
+#############
+A = "Gisma"
+B = "Yale"
+
+print(A == B)
+print(A != B)
