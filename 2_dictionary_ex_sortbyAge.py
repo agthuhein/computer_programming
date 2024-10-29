6bc6b70ed6a66587ccc07e0b9559a50aacbf153d
agthuhein <agthuhein.mm@gmail.com>
2024-10-29 17:05:15 +0100

    Update

--- 2_dictionary_ex_sortbyAge.py
+++ 2_dictionary_ex_sortbyAge.py
@@ -1,6 +1,9 @@
 collection_a = [{"name": "oliver", "age":41}, {"name": "rachel", "age":20}, {"name": "william", "age":30}]
 
-sorted_by_age = sorted(collection_a, key=lambda x:x["age"])
+#get_age = lambda a : a["age"]
+#print(get_age)
+
+sorted_by_age = sorted(collection_a, key=lambda x:x["age"], reverse=False)
 #sorted_by_name = sorted(collection_a, key=lambda x:x["name"])
 
 print(sorted_by_age)
@@ -8,5 +11,9 @@ print(sorted_by_age)
 
 
 ###Lambda function example
-add_two_number = lambda x,y: x+y
-print(add_two_number(5, 6))
+#add_two_number = lambda x,y: x+y
+#print(add_two_number(5, 6))
+
+test = (3, 2, 1, 5, 6, 2, 3, 90)
+sorted_item = sorted(test, reverse=True)
+print(sorted_item)
