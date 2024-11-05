26c2b78762e85bc2ae1a2202bb1b21d931c42984
agthuhein <agthuhein.mm@gmail.com>
2024-11-05 15:08:19 +0100

    Ex 5

---
+++ 4_Ex_Name.py
@@ -0,0 +1,6 @@
+name = input("Enter your name please: ")
+if name == "James Bond":
+    print("We've been expecting you, Mr. Bond.")
+else:
+    #print("Access Denied: {}".format(name))
+    print(F"Access Denied: {name}.")
